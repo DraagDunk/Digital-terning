30f61c9533085de548a3d82d442ee5b3febd3ca3
Jesper Dam <jesper.dk@hotmail.dk>
2022-04-04 10:25:27 +0200

    dark mode ændringer
    Knappen til darkmode/light mode er blevet pænere og centreret. Terningerne bliver nu grå i darkmode.

--- d6_darkmode.js
+++ d6_darkmode.js
@@ -1,17 +1,28 @@
 // Hent elementer for HTML
 const btn = document.querySelector('#dm-button')
 const body = document.querySelector('body')
+const diceFaces = document.querySelectorAll('.dieFace')
 
 function darkMode() {
   body.style.backgroundColor = 'darkslategray';
   mode = 'dark';
   btn.innerText = 'Lyst udtryk';
+  btn.style.color = 'aquamarine';
+  btn.style.borderColor = 'aquamarine';
+  for (let face of diceFaces) {
+    face.style.backgroundColor = 'gray';
+  }
 }
 
 function lightMode() {
   body.style.backgroundColor = 'aquamarine';
   mode = 'light';
   btn.innerText = 'Mørkt udtryk';
+  btn.style.color = 'darkslategray';
+  btn.style.borderColor = 'darkslategray';
+  for (let face of diceFaces) {
+    face.style.backgroundColor = 'white';
+  }
 }
 
 let mode = "light"
